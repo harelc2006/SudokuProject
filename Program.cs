1cb598d5b1f85155362f2174cedaffbbf1b68d46
Harel Cohen <harelcohen2006@gmail.com>
2025-01-21 00:56:34 +0200

    fixed - GetInnerBoxes and created two functions

--- Program.cs
+++ Program.cs
@@ -10,9 +10,6 @@ namespace Sudoku
     {
         static void Main(string[] args)
         {
-            int[,] numbers = { { 1, 4, 2}, { 3, 6, 8 } ,{1,4,2 } };
-            Console.WriteLine(numbers.Length);
-            Console.ReadLine();
         }
     }
 }
